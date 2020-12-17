6bf9304e1bdc58c1a9bc4f93767c4fef70376445
kjolson12 <kobaseball02@gmail.com>
2020-12-16 21:05:00 -0800

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { Grid, Icon, Button } from 'semantic-ui-react';
 
+import Logo from '../images/Violet_Black_logo';
+
 const Navbar = ({ toggleTheme, themeClassName }) => {
     return (
         <Grid padded>
             <Grid.Row>
-                <Grid.Column width={8} verticalAlign='middle'>
+                <Grid.Column width={8}>
+                    <Logo />
+                </Grid.Column>
+                <Grid.Column width={8} textAlign='right'>
+                    <Icon
+                        name={themeClassName('sun', 'moon')}
+                        size='big'
+                        color='violet'
+                    />
                     <Icon
                         name={themeClassName('toggle off', 'toggle on')}
                         size='huge'
                         onClick={toggleTheme}
                         color='violet'
                     />
-                    <Icon
-                        name={themeClassName('sun', 'moon')}
-                        size='big'
-                        color='violet'
-                    />
-                </Grid.Column>
-                <Grid.Column width={8}>
                     <Button
                         color='violet'
                         floated='right'
                         animated
                         href='https://docs.google.com/document/d/1hwFZ9iQUeT3MRrY428W56Vt7OMOV-UB-SPG-A_fPzOw/export?format=pdf'
-                        size='huge'
+                        size='big'
                     >
                         <Button.Content visible>Resumé</Button.Content>
                         <Button.Content hidden>
---
+++ src/images/Violet_Black_logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <svg style={{ height: '3.5rem', width: 'auto' }}id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 497.24 261.53">
+            <path className='cls-1' style={{ fill: '#6435C9' }} d="M0,131.79,95.13,1.16,131.06,1,35.93,131.62l96.33,129.74-35.93.17Z"/>
+            <path className="cls-1" style={{ fill: '#6435C9' }} d="M498.68,131,403,261.14H367L462.74,131,367,.77H403Z"/>
+            <polygon className="cls-1" style={{ fill: '#6435C9' }} points="346.88 0 273.6 123.01 254.11 155.74 191.33 261.09 151.79 261.27 184.57 206.41 234.3 123.19 253.81 90.58 307.83 0.18 346.88 0"/>
+            <polygon points="320.16 261.23 260.99 163.4 280.48 130.49 330.97 213.72 359.68 261.04 320.16 261.23"/>
+            <polygon points="183.62 0.75 184.48 186.8 151.7 241.65 150.59 0.91 183.62 0.75"/>
+        </svg>
+    );
+};
+
+export default Logo;
