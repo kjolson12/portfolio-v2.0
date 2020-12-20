bb634d484da624fdfbaf8db1c627522b61cc25b4
kjolson12 <kobaseball02@gmail.com>
2020-12-19 22:49:44 -0800

    changed image sizes

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -20,7 +20,7 @@ class Skill extends React.Component {
                         <Image
                             alt='logo'
                             src={this.props.imageSource}
-                            size='tiny'
+                            size='small'
                             onMouseOver={this.toggleVisibility}
                             style={{ maxHeight: '80px !important', maxWidth: '80px !important' }}
                         />
