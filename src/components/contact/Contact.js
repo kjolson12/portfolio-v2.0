1e71a9c536eaf308f556fa9a0041173d5dec8aa1
kjolson12 <kobaseball02@gmail.com>
2021-05-06 10:15:39 -0600

    added picture to footer

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Grid, Container, Icon, Header, Divider } from 'semantic-ui-react';
+import { Grid, Container, Image, Icon, Header, Divider } from 'semantic-ui-react';
 import ContactButton from './ContactButton';
+
+import selfie from '../../images/selfie.jpeg';
 
 const Contact = () => {
     return (
         <Grid padded centered>
             <Grid.Row color='violet'>
                 <Container>
+                    <Image
+                        src={selfie}
+                        size='small'
+                        inline
+                        circular
+                        style={{ marginTop: '50px' }}
+                    />
+                    <Icon name='talk' size='huge' />
                     <Header
-                        icon
                         inverted
                         as='h1'
-                        style={{ marginTop: '50px', }}
                     >
-                        <Icon name='talk' />
                         Get in touch
                     </Header>
                     <Divider hidden />
